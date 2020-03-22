96bfa71af6db3c8436d221dc4fc4a27839fff9f0
sjmog <s_morgan@me.com>
2020-03-22 13:46:28 +0000

    Move the card creation into the Deck creation
    * Now the deck draws a card from the cards (still only one type of card)

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,7 +2,11 @@ require_relative './card'
 
 # A collection of cards
 class Deck
+  def initialize
+    @cards = [Card.new(2, :spades)]
+  end
+
   def draw
-    Card.new(2, :spades)
+    @cards.pop
   end
 end
