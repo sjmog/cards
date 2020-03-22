2c5b34727e78bf84d9d6b6201a2ac58c64fee171
sjmog <s_morgan@me.com>
2020-03-22 14:39:06 +0000

    Extract the conditional logic out of #human_value
    * Generally speaking, it's better to minimise conditional logic in an
    application.
    * One way to do this is to use a Lookup (a hash).
    * This is a pattern you can use over and over in programming.
    * Basically, you use the fact that { key => value }[key] returns value.
    * We can therefore extract the human values to a constant.
    * Constants are valuable as they express knowledge in the application in
    an easy-to-read and easy-to-modify way (think about what we did with
    SUITS).

--- lib/card.rb
+++ lib/card.rb
@@ -2,24 +2,29 @@
 class Card
   include Comparable
 
+  HUMAN_VALUES = {
+     1 => :ace,
+     2 => 2,
+     3 => 3,
+     4 => 4,
+     5 => 5,
+     6 => 6,
+     7 => 7,
+     8 => 8,
+     9 => 9,
+    10 => 10,
+    11 => :jack,
+    12 => :queen,
+    13 => :king
+  }
+
   def initialize(value, suit)
     @value = value
     @suit = suit
   end
 
   def human_value
-    case value
-    when 1
-      return :ace
-    when 11
-      return :jack
-    when 12
-      return :queen
-    when 13
-      return :king
-    else
-      return value
-    end
+    HUMAN_VALUES[value]
   end
 
   def to_s
