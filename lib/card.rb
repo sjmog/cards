9355c9cc0ec56b33a68b147646c02d8bd9fe20fb
sjmog <s_morgan@me.com>
2020-03-22 13:50:29 +0000

    Beef up the value function
    * By creating a function that converts the value for certain numbers (1
    is an ace, 11 a jack, and so on), we retain the "innate" value of cards
    as an integerm, but still make cards read in a human way.
    * This will be useful later on when we want to compare cards: each card
    will know its 'value' so we'll be able to compare them numerically.

--- lib/card.rb
+++ lib/card.rb
@@ -1,9 +1,24 @@
 # A single card
 class Card
-  attr_reader :value, :suit
+  attr_reader :suit
 
   def initialize(value, suit)
     @value = value
     @suit = suit
+  end
+
+  def value
+    case @value
+    when 1
+      return :ace
+    when 11
+      return :jack
+    when 12
+      return :queen
+    when 13
+      return :king
+    else
+      return @value
+    end
   end
 end
