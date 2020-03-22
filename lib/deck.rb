b19bb2d8a57cbcaf716e5057dcd1a214153aa0b0
sjmog <s_morgan@me.com>
2020-03-22 14:10:02 +0000

    Refactor the Deck initialization
    * We can do all of the card creation, flattening, and shuffling in one
    line - it's pretty clear.

--- lib/deck.rb
+++ lib/deck.rb
@@ -3,14 +3,12 @@ require_relative './card'
 # A collection of cards
 class Deck
   def initialize
-    cards = [
+    @cards = [
               Card.build(:spades), 
               Card.build(:hearts), 
               Card.build(:diamonds),
               Card.build(:clubs)
-            ].flatten
-
-    @cards = cards.shuffle
+            ].flatten.shuffle
   end
 
   def draw
